3b3f2591b45ceac2d8f93578771d8b390a7e994d
sanao1006 <naosak1006@gmail.com>
2024-10-26 17:12:36 +0900

    Add implementation for LoginScreen and LoginScreenPresenter

--- feature/login/src/main/java/me/sanao1006/feature/login/LoginScreenPresenter.kt
+++ feature/login/src/main/java/me/sanao1006/feature/login/LoginScreenPresenter.kt
@@ -1,15 +1,28 @@
 package me.sanao1006.feature.login
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.presenter.Presenter
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
 @CircuitInject(LoginScreen::class, SingletonComponent::class)
-class LoginScreenPresenter @Inject constructor(): Presenter<LoginScreen.State> {
+class LoginScreenPresenter @Inject constructor() : Presenter<LoginScreen.State> {
     @Composable
     override fun present(): LoginScreen.State {
-        return LoginScreen.State()
+        var domain by rememberRetained { mutableStateOf("") }
+        return LoginScreen.State(
+            domain = domain
+        ) { event ->
+            when (event) {
+                is LoginScreen.Event.OnTextChanged -> {
+                    domain = event.text
+                }
+            }
+        }
     }
 }
