78eb83191244c7193f2a9d941bd4d8082221b3bf
sanao1006 <naosak1006@gmail.com>
2024-10-27 22:10:27 +0900

    rename MiauthApi to MiauthRepository

--- core/network/src/main/java/me/sanao1006/core/network/api/MiauthRepository.kt
+++ core/network/src/main/java/me/sanao1006/core/network/api/MiauthRepository.kt
@@ -3,7 +3,7 @@ package me.sanao1006.core.network.api
 import de.jensklingenberg.ktorfit.http.POST
 import de.jensklingenberg.ktorfit.http.Path
 
-interface MiauthApi {
+interface MiauthRepository {
     @POST("api/miauth/{session}/check")
     fun miauth(@Path("session") session: String)
 }
