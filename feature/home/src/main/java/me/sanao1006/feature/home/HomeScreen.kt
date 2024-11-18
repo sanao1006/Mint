87018014e86a21e40de11e2e10a068e15e5f0efb
sanao1006 <naosak1006@gmail.com>
2024-11-18 23:13:07 +0900

    refactor

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
@@ -1,33 +1,20 @@
 package me.sanao1006.feature.home
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
-import compose.icons.TablerIcons
-import compose.icons.tablericons.Home
-import compose.icons.tablericons.Planet
-import compose.icons.tablericons.World
 import dagger.hilt.components.SingletonComponent
 import kotlinx.parcelize.Parcelize
 import me.sanao1006.core.model.home.notes.TimelineUiState
@@ -52,30 +39,11 @@ fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
     Box(modifier = modifier.fillMaxSize()) {
         Scaffold(
             topBar = {
-                TopAppBar(
-                    title = {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            IconButton(onClick = {}) {
-                                Icon(imageVector = TablerIcons.Home, "")
-                            }
-
-                            IconButton(onClick = {}) {
-                                Icon(imageVector = TablerIcons.World, "")
-                            }
-
-                            IconButton(onClick = {}) {
-                                Icon(imageVector = TablerIcons.Planet, "")
-                            }
-                        }
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = { }) {
-                            Icon(imageVector = Icons.Default.Menu, "")
-                        }
-                    }
+                HomeScreenTopAppBar(
+                    onNavigationIconClick = {},
+                    onHomeClick = {},
+                    onSocialClick = {},
+                    onGlobalClick = {}
                 )
             }
         ) {
---
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenTopAppBar.kt
@@ -0,0 +1,67 @@
+package me.sanao1006.feature.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import compose.icons.TablerIcons
+import compose.icons.tablericons.Home
+import compose.icons.tablericons.Planet
+import compose.icons.tablericons.World
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenTopAppBar(
+    modifier: Modifier = Modifier,
+    onNavigationIconClick: () -> Unit,
+    onHomeClick: () -> Unit,
+    onSocialClick: () -> Unit,
+    onGlobalClick: () -> Unit
+) {
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                IconButton(onClick = onHomeClick) {
+                    Icon(imageVector = TablerIcons.Home, "")
+                }
+
+                IconButton(onClick = onSocialClick) {
+                    Icon(imageVector = TablerIcons.World, "")
+                }
+
+                IconButton(onClick = onGlobalClick) {
+                    Icon(imageVector = TablerIcons.Planet, "")
+                }
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(imageVector = Icons.Default.Menu, "")
+            }
+        }
+    )
+}
+
+@PreviewLightDark
+@Composable
+fun PreviewHomeScreenTopAppBar() {
+    HomeScreenTopAppBar(
+        onNavigationIconClick = {},
+        onHomeClick = {},
+        onSocialClick = {},
+        onGlobalClick = {}
+    )
+}
