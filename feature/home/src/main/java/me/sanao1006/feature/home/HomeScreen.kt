8428eff7a5b3f579cbf632bec544102f34c1908c
sanao1006 <naosak1006@gmail.com>
2024-11-18 22:09:03 +0900

    wip: Add HomeScreen UI with TopAppBar

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
@@ -1,17 +1,26 @@
 package me.sanao1006.feature.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
@@ -40,9 +49,28 @@ fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
     Box(modifier = modifier.fillMaxSize()) {
         Scaffold(
             topBar = {
-                CenterAlignedTopAppBar(
+                TopAppBar(
                     title = {
-                        Text("Home")
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            IconButton(onClick = {}) {
+                                Icon(imageVector = Icons.Default.Home, "")
+                            }
+
+                            IconButton(onClick = {}) {
+                                Icon(imageVector = Icons.Default.Home, "")
+                            }
+                            IconButton(onClick = {}) {
+                                Icon(imageVector = Icons.Default.Home, "")
+                            }
+                        }
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { }) {
+                            Icon(imageVector = Icons.Default.Menu, "")
+                        }
                     }
                 )
             }
