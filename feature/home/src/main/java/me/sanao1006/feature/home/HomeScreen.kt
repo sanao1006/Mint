4de143113e2511dea2731188b519af36af692ec8
sanao1006 <naosak1006@gmail.com>
2024-11-18 21:01:32 +0900

    Add HomeScreenPresenter with timeline state management using Flow and Compose

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
@@ -2,6 +2,12 @@ package me.sanao1006.feature.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
@@ -12,12 +18,13 @@ import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
 import dagger.hilt.components.SingletonComponent
 import kotlinx.parcelize.Parcelize
+import me.sanao1006.core.model.home.notes.TimelineUiState
 
 @Parcelize
 data object HomeScreen : Screen {
     @Immutable
     data class State(
-        val title: String = "Home",
+        val uiState: List<TimelineUiState?> = listOf(),
         val eventSink: (Event) -> Unit
     ) : CircuitUiState
 
@@ -26,10 +33,26 @@ data object HomeScreen : Screen {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @CircuitInject(HomeScreen::class, SingletonComponent::class)
 @Composable
 fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
     Box(modifier = modifier.fillMaxSize()) {
-        Text("hello world")
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text("Home")
+                    }
+                )
+            }
+        ) {
+            LazyColumn(modifier = Modifier.padding(it)) {
+                items(state.uiState) {
+                    Text(text = it?.user?.name ?: "")
+                    Text(text = it?.text ?: "")
+                }
+            }
+        }
     }
 }
--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenPresenter.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenPresenter.kt
@@ -1,20 +1,38 @@
 package me.sanao1006.feature.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.presenter.Presenter
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import me.sanao1006.core.data.repository.NotesRepository
+import me.sanao1006.core.model.home.notes.NotesTimeLineRequestBody
+import me.sanao1006.datastore.DataStoreRepository
 import javax.inject.Inject
 
 @CircuitInject(HomeScreen::class, SingletonComponent::class)
 class HomeScreenPresenter @Inject constructor(
-    
+    private val notesRepository: NotesRepository,
+    private val dataStoreRepository: DataStoreRepository
 ) : Presenter<HomeScreen.State> {
+    @OptIn(ExperimentalCoroutinesApi::class)
+
+    private val _timelineUiState = dataStoreRepository.flowAccessToken().flatMapLatest {
+        notesRepository.getNotesTimeline(
+            notesTimeLineRequestBody = NotesTimeLineRequestBody(i = it)
+        )
+    }.map { notesTimeline ->
+        notesTimeline.map { it.toTimelineUiState() }
+    }
 
     @Composable
     override fun present(): HomeScreen.State {
+        val timelineUiState = _timelineUiState.collectAsState(initial = emptyList()).value
         return HomeScreen.State(
-            title = "Home",
+            uiState = timelineUiState,
             eventSink = {}
         )
     }
