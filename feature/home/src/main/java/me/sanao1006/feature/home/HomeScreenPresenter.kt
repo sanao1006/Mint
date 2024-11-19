705feca939a8a30328715a53fe8cbd16733adce6
sanao1006 <naosak1006@gmail.com>
2024-11-19 22:20:30 +0900

    refactor: use UseCase

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenPresenter.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenPresenter.kt
@@ -5,32 +5,16 @@ import androidx.compose.runtime.collectAsState
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.presenter.Presenter
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
-import me.sanao1006.core.data.repository.NotesRepository
-import me.sanao1006.core.model.home.notes.NotesTimeLineRequestBody
-import me.sanao1006.datastore.DataStoreRepository
+import me.sanao1006.feature.home.domain.FlowNotesTimelineUseCase
 import javax.inject.Inject
 
 @CircuitInject(HomeScreen::class, SingletonComponent::class)
 class HomeScreenPresenter @Inject constructor(
-    private val notesRepository: NotesRepository,
-    private val dataStoreRepository: DataStoreRepository
+    private val flowNotesTimelineUseCase: FlowNotesTimelineUseCase
 ) : Presenter<HomeScreen.State> {
-    @OptIn(ExperimentalCoroutinesApi::class)
-
-    private val _timelineUiState = dataStoreRepository.flowAccessToken().flatMapLatest {
-        notesRepository.getNotesTimeline(
-            notesTimeLineRequestBody = NotesTimeLineRequestBody(i = it)
-        )
-    }.map { notesTimeline ->
-        notesTimeline.map { it.toTimelineUiState() }
-    }
-
     @Composable
     override fun present(): HomeScreen.State {
-        val timelineUiState = _timelineUiState.collectAsState(initial = emptyList()).value
+        val timelineUiState = flowNotesTimelineUseCase().collectAsState(initial = emptyList()).value
         return HomeScreen.State(
             uiState = timelineUiState,
             eventSink = {}
--- feature/home/src/main/java/me/sanao1006/feature/home/domain/FlowNotesTimelineUseCase.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/domain/FlowNotesTimelineUseCase.kt
@@ -1,6 +1,12 @@
 package me.sanao1006.feature.home.domain
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import me.sanao1006.core.data.repository.NotesRepository
+import me.sanao1006.core.model.home.notes.NotesTimeLineRequestBody
+import me.sanao1006.core.model.home.notes.TimelineUiState
 import me.sanao1006.datastore.DataStoreRepository
 import javax.inject.Inject
 
@@ -8,4 +14,14 @@ class FlowNotesTimelineUseCase @Inject constructor(
     private val notesRepository: NotesRepository,
     private val dataStoreRepository: DataStoreRepository
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<List<TimelineUiState>> {
+        return dataStoreRepository.flowAccessToken().flatMapLatest {
+            notesRepository.getNotesTimeline(
+                notesTimeLineRequestBody = NotesTimeLineRequestBody(i = it)
+            )
+        }.map { notesTimeline ->
+            notesTimeline.map { it.toTimelineUiState() }
+        }
+    }
 }
