fb96aeae88cb69e83745c9b35e8c063a1ab55be0
sanao1006 <naosak1006@gmail.com>
2024-11-24 22:48:17 +0900

    create error response

---
+++ core/model/src/main/java/me/sanao1006/core/model/Error.kt
@@ -0,0 +1,15 @@
+package me.sanao1006.core.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Error(
+    @SerialName("code")
+    val code: String = "",
+    @SerialName("id")
+    val id: String = "",
+    @SerialName("message")
+    val message: String = ""
+)
