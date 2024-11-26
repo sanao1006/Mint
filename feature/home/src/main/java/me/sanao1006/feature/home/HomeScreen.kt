6074b8cdaa7f988738abbed54ce02997fc756769
sanao1006 <naosak1006@gmail.com>
2024-11-26 15:04:30 +0900

    pager

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
@@ -1,21 +1,26 @@
 package me.sanao1006.feature.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import me.sanao1006.core.model.home.notes.TimelineUiState
 
@@ -37,20 +42,40 @@ data object HomeScreen : Screen {
 @Composable
 fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
     Box(modifier = modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState(initialPage = 0) { 3 }
+        val scope = rememberCoroutineScope()
         Scaffold(
             topBar = {
                 HomeScreenTopAppBar(
                     onNavigationIconClick = {},
-                    onHomeClick = {},
-                    onSocialClick = {},
-                    onGlobalClick = {}
+                    onHomeClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(0)
+                        }
+                    },
+                    onSocialClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(1)
+                        }
+                    },
+                    onGlobalClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(2)
+                        }
+                    }
                 )
             }
         ) {
-            LazyColumn(modifier = Modifier.padding(it)) {
-                items(state.uiState) {
-                    Text(text = it?.user?.name ?: "")
-                    Text(text = it?.text ?: "")
+            Column(modifier = Modifier.padding(it)) {
+                HorizontalPager(
+                    state = pagerState
+                ) { page ->
+                    LazyColumn {
+                        items(state.uiState) {
+                            Text(text = it?.user?.name ?: "")
+                            Text(text = it?.text ?: "")
+                        }
+                    }
                 }
             }
         }
