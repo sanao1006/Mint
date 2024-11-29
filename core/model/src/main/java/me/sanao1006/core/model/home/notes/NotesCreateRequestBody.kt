b9cd1d5dd6e74c0fe022e92d05a2614fa62d1aeb
sanao1006 <naosak1006@gmail.com>
2024-11-29 22:53:20 +0900

    fix

--- core/model/src/main/java/me/sanao1006/core/model/home/notes/NotesCreateRequestBody.kt
+++ core/model/src/main/java/me/sanao1006/core/model/home/notes/NotesCreateRequestBody.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 data class NotesCreateRequestBody(
     val text: String,
     val visibility: Visibility,
-    val visibleUserIds: List<String?> = listOf(null),
+    val visibleUserIds: List<String>? = null,
     val cw: String? = null,
     val localOnly: Boolean = false,
     val reactionAcceptance: ReactionAcceptance,
@@ -16,7 +16,7 @@ data class NotesCreateRequestBody(
     val replyId: String? = null,
     val renoteId: String? = null,
     val channelId: String? = null,
-    val fileIds: List<String?> = listOf(null),
+    val fileIds: List<String>? = null,
     val poll: Poll? = null,
-    val mediaIds: List<String?> = listOf(null)
+    val mediaIds: List<String>? = null
 )
