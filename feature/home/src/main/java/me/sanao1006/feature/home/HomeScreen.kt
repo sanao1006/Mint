105886f1327d6407d3402c6d5c1891d46ac86898
sanao1006 <naosak1006@gmail.com>
2024-12-03 00:20:16 +0900

    feat: topAppBar nested scroll

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreen.kt
@@ -12,16 +12,20 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
@@ -32,6 +36,7 @@ import me.sanao1006.core.ui.MainScreenBottomAppBar
 import me.sanao1006.screens.HomeScreen
 import me.sanao1006.screens.MainScreenType
 
+@OptIn(ExperimentalMaterial3Api::class)
 @CircuitInject(HomeScreen::class, SingletonComponent::class)
 @Composable
 fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
@@ -40,6 +45,7 @@ fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
         val scope = rememberCoroutineScope()
         val drawerState = rememberDrawerState(DrawerValue.Closed)
         val snackbarHostState = remember { SnackbarHostState() }
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
         LaunchedEffect(state.isSuccessCreateNote) {
             state.eventSink(HomeScreen.Event.OnNoteCreated(snackbarHostState, scope))
         }
@@ -50,6 +56,8 @@ fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
             HomeScreenUiContent(
                 state = state,
                 pagerState = pagerState,
+                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                scrollBehavior = scrollBehavior,
                 snackbarHostState = { SnackbarHost(hostState = snackbarHostState) },
                 onNavigationIconClick = {
                     scope.launch {
@@ -90,10 +98,13 @@ fun HomeScreenUi(state: HomeScreen.State, modifier: Modifier) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeScreenUiContent(
     state: HomeScreen.State,
     pagerState: PagerState,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior,
     snackbarHostState: @Composable () -> Unit,
     onNavigationIconClick: () -> Unit,
     onHomeClick: () -> Unit,
@@ -102,8 +113,10 @@ private fun HomeScreenUiContent(
     floatingActionButton: @Composable () -> Unit
 ) {
     Scaffold(
+        modifier = modifier,
         topBar = {
             HomeScreenTopAppBar(
+                scrollBehavior = scrollBehavior,
                 onNavigationIconClick = onNavigationIconClick,
                 onHomeClick = onHomeClick,
                 onSocialClick = onSocialClick,
--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenTopAppBar.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenTopAppBar.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -17,6 +19,7 @@ import ir.alirezaivaz.tablericons.TablerIcons
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenTopAppBar(
+    scrollBehavior: TopAppBarScrollBehavior,
     modifier: Modifier = Modifier,
     onNavigationIconClick: () -> Unit,
     onHomeClick: () -> Unit,
@@ -24,6 +27,7 @@ fun HomeScreenTopAppBar(
     onGlobalClick: () -> Unit
 ) {
     TopAppBar(
+        scrollBehavior = scrollBehavior,
         modifier = modifier,
         title = {
             Row(
@@ -51,10 +55,12 @@ fun HomeScreenTopAppBar(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @PreviewLightDark
 @Composable
 fun PreviewHomeScreenTopAppBar() {
     HomeScreenTopAppBar(
+        scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
         onNavigationIconClick = {},
         onHomeClick = {},
         onSocialClick = {},
