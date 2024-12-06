24f98633b872202a003c38580fa2eb1429d08330
sanao1006 <naosak1006@gmail.com>
2024-12-07 01:12:43 +0900

    use string resource

--- feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenDrawer.kt
+++ feature/home/src/main/java/me/sanao1006/feature/home/HomeScreenDrawer.kt
@@ -1,5 +1,7 @@
 package me.sanao1006.feature.home
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -35,137 +37,137 @@ import me.snao1006.res_value.ResString
 
 @Composable
 fun HomeScreenDrawer(
-  loginUserInfo: LoginUserInfo,
-  modifier: Modifier = Modifier,
-  drawerState: DrawerState,
-  onDrawerFavoriteClick: () -> Unit,
-  onDrawerAnnouncementClick: () -> Unit,
-  onDrawerClipClick: () -> Unit,
-  onDrawerAntennaClick: () -> Unit,
-  onDrawerExploreClick: () -> Unit,
-  onDrawerChannelClick: () -> Unit,
-  onDrawerDriveClick: () -> Unit,
-  onDrawerGalleryClick: () -> Unit,
-  onDrawerAboutClick: () -> Unit,
-  onDrawerAccountPreferencesClick: () -> Unit,
-  onDrawerSettingsClick: () -> Unit,
-  content: @Composable () -> Unit
+    loginUserInfo: LoginUserInfo,
+    modifier: Modifier = Modifier,
+    drawerState: DrawerState,
+    onDrawerFavoriteClick: () -> Unit,
+    onDrawerAnnouncementClick: () -> Unit,
+    onDrawerClipClick: () -> Unit,
+    onDrawerAntennaClick: () -> Unit,
+    onDrawerExploreClick: () -> Unit,
+    onDrawerChannelClick: () -> Unit,
+    onDrawerDriveClick: () -> Unit,
+    onDrawerGalleryClick: () -> Unit,
+    onDrawerAboutClick: () -> Unit,
+    onDrawerAccountPreferencesClick: () -> Unit,
+    onDrawerSettingsClick: () -> Unit,
+    content: @Composable () -> Unit
 ) {
-  ModalNavigationDrawer(
-    modifier = modifier,
-    drawerState = drawerState,
-    drawerContent = {
-      ModalDrawerSheet {
-        LoginUserInfo(
-          modifier = Modifier.padding(start = 16.dp, end = 24.dp),
-          loginUserInfo = loginUserInfo
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        HorizontalDivider(
-          modifier = Modifier
-            .fillMaxWidth(0.9f)
-            .align(Alignment.CenterHorizontally)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        DrawerItem.entries.forEach {
-          NavigationDrawerItem(
-            icon = { Icon(painter = painterResource(it.resId), "") },
-            label = { Text(it.title) },
-            selected = false,
-            onClick = when (it) {
-              DrawerItem.FAVORITE -> onDrawerFavoriteClick
-              DrawerItem.ANNOUNCEMENT -> onDrawerAnnouncementClick
-              DrawerItem.CLIP -> onDrawerClipClick
-              DrawerItem.ANTENNA -> onDrawerAntennaClick
-              DrawerItem.EXPLORE -> onDrawerExploreClick
-              DrawerItem.CHANNEL -> onDrawerChannelClick
-              DrawerItem.DRIVE -> onDrawerDriveClick
-              DrawerItem.GALLERY -> onDrawerGalleryClick
-              DrawerItem.ABOUT -> onDrawerAboutClick
-              DrawerItem.ACCOUNT_PREFERENCES -> onDrawerAccountPreferencesClick
-              DrawerItem.SETTINGS -> onDrawerSettingsClick
+    ModalNavigationDrawer(
+        modifier = modifier,
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                LoginUserInfo(
+                    modifier = Modifier.padding(start = 16.dp, end = 24.dp),
+                    loginUserInfo = loginUserInfo
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                HorizontalDivider(
+                    modifier = Modifier
+                      .fillMaxWidth(0.9f)
+                      .align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                DrawerItem.entries.forEach {
+                    NavigationDrawerItem(
+                        icon = { Icon(painter = painterResource(it.iconId), "") },
+                        label = { Text(text = stringResource(it.titleId)) },
+                        selected = false,
+                        onClick = when (it) {
+                            DrawerItem.FAVORITE -> onDrawerFavoriteClick
+                            DrawerItem.ANNOUNCEMENT -> onDrawerAnnouncementClick
+                            DrawerItem.CLIP -> onDrawerClipClick
+                            DrawerItem.ANTENNA -> onDrawerAntennaClick
+                            DrawerItem.EXPLORE -> onDrawerExploreClick
+                            DrawerItem.CHANNEL -> onDrawerChannelClick
+                            DrawerItem.DRIVE -> onDrawerDriveClick
+                            DrawerItem.GALLERY -> onDrawerGalleryClick
+                            DrawerItem.ABOUT -> onDrawerAboutClick
+                            DrawerItem.ACCOUNT_PREFERENCES -> onDrawerAccountPreferencesClick
+                            DrawerItem.SETTINGS -> onDrawerSettingsClick
+                        }
+                    )
+                }
             }
-          )
-        }
-      }
-    },
-    content = content
-  )
+        },
+        content = content
+    )
 }
 
 @Composable
 private fun LoginUserInfo(
-  loginUserInfo: LoginUserInfo,
-  modifier: Modifier = Modifier
+    loginUserInfo: LoginUserInfo,
+    modifier: Modifier = Modifier
 ) {
-  Column(
-    modifier = modifier,
-    horizontalAlignment = Alignment.Start
-  ) {
-    Spacer(modifier = Modifier.height(48.dp))
-    Image(
-      painter = rememberAsyncImagePainter(loginUserInfo.avatarUrl),
-      contentDescription = null,
-      modifier = Modifier
-        .clip(CircleShape)
-        .size(64.dp),
-      contentScale = ContentScale.Crop
-    )
-    Spacer(modifier = Modifier.height(4.dp))
-    Text(
-      text = loginUserInfo.userName,
-      style = MaterialTheme.typography.headlineSmall
-    )
-    if (loginUserInfo.name.isNotEmpty()) {
-      Spacer(modifier = Modifier.height(2.dp))
-      Text(
-        text = loginUserInfo.name,
-        style = MaterialTheme.typography.labelSmall
-      )
-    }
-    Spacer(modifier = Modifier.height(8.dp))
-
-    Row(
-      modifier = Modifier.wrapContentHeight(align = Alignment.Bottom),
-      verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start
     ) {
-      Text(
-        text = loginUserInfo.followingCount.toString(),
-        style = MaterialTheme.typography.bodyLarge,
-        fontWeight = FontWeight.Bold
-      )
-      Spacer(modifier = Modifier.width(4.dp))
-      Text(
-        text = stringResource(ResString.drawer_following),
-        style = MaterialTheme.typography.labelLarge
-      )
+        Spacer(modifier = Modifier.height(48.dp))
+        Image(
+            painter = rememberAsyncImagePainter(loginUserInfo.avatarUrl),
+            contentDescription = null,
+            modifier = Modifier
+              .clip(CircleShape)
+              .size(64.dp),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = loginUserInfo.userName,
+            style = MaterialTheme.typography.headlineSmall
+        )
+        if (loginUserInfo.name.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(
+                text = loginUserInfo.name,
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
 
-      Spacer(modifier = Modifier.width(10.dp))
+        Row(
+            modifier = Modifier.wrapContentHeight(align = Alignment.Bottom),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = loginUserInfo.followingCount.toString(),
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(ResString.drawer_following),
+                style = MaterialTheme.typography.labelLarge
+            )
 
-      Text(
-        text = loginUserInfo.followersCount.toString(),
-        style = MaterialTheme.typography.bodyLarge,
-        fontWeight = FontWeight.Bold
-      )
-      Spacer(modifier = Modifier.width(4.dp))
-      Text(
-        text = stringResource(ResString.drawer_followers),
-        style = MaterialTheme.typography.labelLarge
-      )
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Text(
+                text = loginUserInfo.followersCount.toString(),
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(ResString.drawer_followers),
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
     }
-  }
 }
 
-enum class DrawerItem(val resId: Int, val title: String) {
-  FAVORITE(TablerIcons.Star, "Favorite"),
-  ANNOUNCEMENT(TablerIcons.Speakerphone, "Announcement"),
-  CLIP(TablerIcons.Paperclip, "Clip"),
-  ANTENNA(TablerIcons.Antenna, "Antenna"),
-  EXPLORE(TablerIcons.Hash, "Explore"),
-  CHANNEL(TablerIcons.DeviceTv, "Channel"),
-  DRIVE(TablerIcons.BrandOnedrive, "Drive"),
-  GALLERY(TablerIcons.Icons, "Gallery"),
-  ABOUT(TablerIcons.InfoCircle, "About"),
-  ACCOUNT_PREFERENCES(TablerIcons.User, "Account Preferences"),
-  SETTINGS(TablerIcons.Settings, "Settings")
+enum class DrawerItem(@DrawableRes val iconId: Int, @StringRes val titleId: Int) {
+    FAVORITE(TablerIcons.Star, ResString.drawer_item_favorite),
+    ANNOUNCEMENT(TablerIcons.Speakerphone, ResString.drawer_item_announcement),
+    CLIP(TablerIcons.Paperclip, ResString.drawer_item_clip),
+    ANTENNA(TablerIcons.Antenna, ResString.drawer_item_antenna),
+    EXPLORE(TablerIcons.Hash, ResString.drawer_item_explore),
+    CHANNEL(TablerIcons.DeviceTv, ResString.drawer_item_channel),
+    DRIVE(TablerIcons.BrandOnedrive, ResString.drawer_item_drive),
+    GALLERY(TablerIcons.Icons, ResString.drawer_item_gallery),
+    ABOUT(TablerIcons.InfoCircle, ResString.drawer_item_about),
+    ACCOUNT_PREFERENCES(TablerIcons.User, ResString.drawer_item_account_preferences),
+    SETTINGS(TablerIcons.Settings, ResString.drawer_item_settings);
 }
