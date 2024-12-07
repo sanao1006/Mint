04341f2962b948dc947ca908c8d93882f61710a2
sanao1006 <naosak1006@gmail.com>
2024-12-07 21:05:08 +0900

    feat: add domain module
    Added the domain module to the project. This module contains the core business logic of the application. It depends on the `network` and `model` modules.

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("mint.convention.androidLibrary")
+    id("mint.convention.ktorPlugin")
+    id("mint.convention.hilt")
+    id("mint.convention.circuit")
+    id("mint.convention.spotless")
+}
+
+dependencies {
+    implementation(projects.core.network)
+    implementation(projects.core.model)
+}
