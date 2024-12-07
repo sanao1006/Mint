e46b73085eb487e155f9ee0ba6d70f2cc989fdc5
sanao1006 <naosak1006@gmail.com>
2024-12-07 23:05:22 +0900

    refactor: migrate `Icon` to material3 package
    Migrate `Icon` composable from `androidx.compose.material` to `androidx.compose.material3` package to ensure consistency and utilize the updated Material Design 3 components.

--- feature/user/src/main/java/me/sanao1006/feature/user/UserScreen.kt
+++ feature/user/src/main/java/me/sanao1006/feature/user/UserScreen.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
