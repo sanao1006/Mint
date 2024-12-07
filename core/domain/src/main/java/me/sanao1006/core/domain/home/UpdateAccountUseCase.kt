ac31484b667d4cf9c0ef1b3e3667a8683fd388db
sanao1006 <naosak1006@gmail.com>
2024-12-07 23:01:54 +0900

    refactor

--- core/domain/src/main/java/me/sanao1006/core/domain/home/UpdateAccountUseCase.kt
+++ core/domain/src/main/java/me/sanao1006/core/domain/home/UpdateAccountUseCase.kt
@@ -1,19 +1,16 @@
 package me.sanao1006.core.domain.home
 
-import javax.inject.Inject
-import kotlinx.serialization.json.Json
 import me.sanao1006.core.data.repository.AccountRepository
 import me.sanao1006.core.model.LoginUserInfo
 import me.sanao1006.datastore.DataStoreRepository
+import javax.inject.Inject
 
 class UpdateAccountUseCase @Inject constructor(
     private val accountRepository: AccountRepository,
     private val dataStoreRepository: DataStoreRepository
 ) {
     suspend operator fun invoke(): LoginUserInfo {
-        val user = accountRepository.i(
-            body = Json.decodeFromString("{}")
-        )
+        val user = accountRepository.i()
         return LoginUserInfo(
             userName = user.username,
             name = user.name ?: "",
