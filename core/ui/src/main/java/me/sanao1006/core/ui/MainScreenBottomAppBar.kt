91222d6b6e6b71cd9ee7dcd5864a147457714248
sanao1006 <naosak1006@gmail.com>
2024-12-11 19:38:48 +0900

    refactor: update bottom bar and search screen to use material3 components
    Updated the bottom bar and search screen to utilize Material3 components for enhanced UI consistency and modern design. This involved replacing Material components with their Material3 counterparts and adjusting the code accordingly.

--- core/ui/src/main/java/me/sanao1006/core/ui/MainScreenBottomAppBar.kt
+++ core/ui/src/main/java/me/sanao1006/core/ui/MainScreenBottomAppBar.kt
@@ -1,8 +1,10 @@
 package me.sanao1006.core.ui
 
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -40,7 +42,16 @@ private fun MainScreenBottomAppBar(
         actions = {
             BottomSheetType.entries.forEach {
                 BottomNavigationItem(
-                    icon = { Icon(painter = painterResource(it.resId), contentDescription = null) },
+                    icon = {
+                        Icon(
+                            painter = painterResource(it.resId),
+                            contentDescription = null,
+                            tint = if (it.mainSheetType == mainSheetType)
+                                MaterialTheme.colorScheme.primary
+                            else
+                                LocalContentColor.current
+                        )
+                    },
                     selected = it.mainSheetType == mainSheetType,
                     onClick = {
                         if (it.mainSheetType != mainSheetType) {
--- feature/search/src/main/java/me/sanao1006/feature/search/SearchScreen.kt
+++ feature/search/src/main/java/me/sanao1006/feature/search/SearchScreen.kt
@@ -4,13 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
