85cce7d2db72915fd23ca64556a97576aa10076f
sanao1006 <naosak1006@gmail.com>
2024-12-12 20:33:56 +0900

    feat: display search screen title
    This commit adds a title to the search screen's top app bar. The title is set to "Search" and is displayed in the center of the app bar.

--- feature/search/src/main/java/me/sanao1006/feature/search/SearchScreen.kt
+++ feature/search/src/main/java/me/sanao1006/feature/search/SearchScreen.kt
@@ -4,45 +4,34 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.slack.circuit.codegen.annotations.CircuitInject
 import dagger.hilt.components.SingletonComponent
 import ir.alirezaivaz.tablericons.TablerIcons
-import me.sanao1006.core.ui.MainScreenDrawerWrapper
 import me.sanao1006.screens.SearchScreen
 import me.sanao1006.screens.event.GlobalIconEvent
+import me.snao1006.res_value.ResString
 
 @CircuitInject(SearchScreen::class, SingletonComponent::class)
 @Composable
 fun SearchScreenUi(state: SearchScreen.State, modifier: Modifier) {
-    val scope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
     Box(modifier = modifier.fillMaxSize()) {
-        MainScreenDrawerWrapper(
-            loginUserInfo = state.loginUserInfo,
-            drawerState = drawerState,
-            scope = scope,
-            event = state.drawerEventSink
-        ) {
-            SearchScreenUiContent(
-                state = state,
-                onGlobalClick = {
-                    state.globalIconEventSink(GlobalIconEvent.OnArrowBackIconClicked)
-                },
-                modifier = modifier
-            )
-        }
+        SearchScreenUiContent(
+            state = state,
+            onGlobalClick = {
+                state.globalIconEventSink(GlobalIconEvent.OnArrowBackIconClicked)
+            },
+            modifier = modifier
+        )
     }
 }
 
@@ -57,7 +46,7 @@ private fun SearchScreenUiContent(
         modifier = modifier,
         topBar = {
             TopAppBar(
-                title = {},
+                title = { Text(stringResource(ResString.drawer_item_search)) },
                 navigationIcon = {
                     IconButton(onClick = onGlobalClick) {
                         Icon(
