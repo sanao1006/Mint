87ecefd42964be3c0a444fdbb51ffca1723df79b
sanao1006 <naosak1006@gmail.com>
2025-01-01 21:17:38 +0900

    feat: add logout feature
    Added a logout feature that clears user data and navigates to the login screen.
    Also added error handling for logout failures.

--- core/screens/src/main/java/me/sanao1006/screens/event/drawer/DrawerEventPresenter.kt
+++ core/screens/src/main/java/me/sanao1006/screens/event/drawer/DrawerEventPresenter.kt
@@ -1,22 +1,30 @@
 package me.sanao1006.screens.event.drawer
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuitx.effects.LaunchedImpressionEffect
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 import me.sanao1006.core.data.compositionLocal.LocalNavigator
+import me.sanao1006.core.data.util.suspendRunCatching
+import me.sanao1006.core.domain.home.LogOutUseCase
 import me.sanao1006.core.domain.home.UpdateAccountUseCase
 import me.sanao1006.core.model.LoginUserInfo
 import me.sanao1006.screens.AnnouncementScreen
 import me.sanao1006.screens.AntennaScreen
 import me.sanao1006.screens.FavoritesScreen
+import me.sanao1006.screens.LoginScreen
 import me.sanao1006.screens.SearchScreen
 import me.sanao1006.screens.UserScreen
+import me.snao1006.res_value.ResString
 
 data class DrawerState(
     val loginUserInfo: LoginUserInfo,
@@ -25,11 +33,14 @@ data class DrawerState(
 ) : CircuitUiState
 
 class DrawerEventPresenter @Inject constructor(
-    private val updateAccountUseCase: UpdateAccountUseCase
+    private val updateAccountUseCase: UpdateAccountUseCase,
+    private val logOutUseCase: LogOutUseCase
 ) : Presenter<DrawerState> {
     @Composable
     override fun present(): DrawerState {
         val navigator = LocalNavigator.current
+        val context = LocalContext.current
+        val scope = rememberCoroutineScope()
         var loginUserInfo: LoginUserInfo by rememberRetained {
             mutableStateOf(
                 LoginUserInfo()
@@ -77,7 +88,22 @@ class DrawerEventPresenter @Inject constructor(
                 }
 
                 DrawerEvent.OnLogOutConfirmClicked -> {
-                    expandDialog = false
+                    scope.launch {
+                        suspendRunCatching {
+                            logOutUseCase.invoke()
+                        }
+                            .onSuccess {
+                                navigator.resetRoot(LoginScreen)
+                            }
+                            .onFailure {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(ResString.log_out_failed),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        expandDialog = false
+                    }
                 }
 
                 DrawerEvent.OnDismissRequest -> {
