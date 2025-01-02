4e53b61c00899c6be023d61a6729e09f75a133df
sanao1006 <naosak1006@gmail.com>
2025-01-02 11:23:27 +0900

    feat: Navigate to home screen when logged in
    This change modifies the navigation logic to automatically navigate to the home screen if the user is logged in when the app starts.
    
    It achieves this by checking the login status within a Composable function and using Circuit's navigation capabilities to navigate to the appropriate screen (home or login) based on this status.

--- app/src/main/java/me/sanao1006/mint/MainActivity.kt
+++ app/src/main/java/me/sanao1006/mint/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.sanao1006.mint
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,21 +8,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.NavigableCircuitContent
 import com.slack.circuit.foundation.rememberCircuitNavigator
 import com.slack.circuit.overlay.ContentWithOverlays
+import com.slack.circuit.runtime.screen.Screen
 import com.slack.circuitx.android.rememberAndroidScreenAwareNavigator
 import com.slack.circuitx.gesturenavigation.GestureNavigationDecoration
 import dagger.hilt.android.AndroidEntryPoint
@@ -45,35 +44,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             MintTheme {
                 window.navigationBarColor = MaterialTheme.colorScheme.background.toArgb()
-                val uiState = viewModel.uiState.collectAsState()
+                val uiState = viewModel.uiState.collectAsStateWithLifecycle()
                 splashscreen.setKeepOnScreenCondition { !uiState.value.tokenLoadingState }
 
                 if (uiState.value.tokenLoadingState) {
-                    val backstack = rememberSaveableBackStack(
-                        if (uiState.value.isLoggedIn) HomeScreen
-                        else LoginScreen
-                    )
-                    val circuitNavigator = rememberCircuitNavigator(backstack)
-                    val navigator = rememberAndroidScreenAwareNavigator(circuitNavigator, this)
-
-                    CompositionLocalProvider(
-                        LocalContext provides this,
-                        LocalNavigator provides navigator
-                    ) {
-                        CircuitCompositionLocals(circuit) {
-                            ContentWithOverlays {
-                                Surface {
-                                    NavigableCircuitContent(
-                                        navigator = navigator,
-                                        backStack = backstack,
-                                        circuit = circuit,
-                                        decoration = GestureNavigationDecoration {
-                                            navigator.pop()
-                                        }
-                                    )
-                                }
-                            }
-                        }
+                    if (uiState.value.isLoggedIn) {
+                        SetupNavigator(circuit, HomeScreen, this)
+                    } else {
+                        SetupNavigator(circuit, LoginScreen, this)
                     }
                 }
             }
@@ -82,17 +60,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun SetupNavigator(
+    circuit: Circuit,
+    screen: Screen,
+    context: Context
+) {
+    val backstack = rememberSaveableBackStack(screen)
+    val circuitNavigator = rememberCircuitNavigator(backstack)
+    val navigator = rememberAndroidScreenAwareNavigator(circuitNavigator, context)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MintTheme {
-        Greeting("Android")
+    CompositionLocalProvider(
+        LocalContext provides context,
+        LocalNavigator provides navigator
+    ) {
+        CircuitCompositionLocals(circuit) {
+            ContentWithOverlays {
+                Surface {
+                    NavigableCircuitContent(
+                        navigator = navigator,
+                        backStack = backstack,
+                        circuit = circuit,
+                        decoration = GestureNavigationDecoration {
+                            navigator.pop()
+                        }
+                    )
+                }
+            }
+        }
     }
 }
