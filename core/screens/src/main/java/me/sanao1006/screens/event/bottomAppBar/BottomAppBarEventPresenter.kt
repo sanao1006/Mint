364f8f4bdb98ec29cb681654112c4d9ddd16d8de
sanao1006 <naosak1006@gmail.com>
2025-01-06 21:47:07 +0900

    refactor: move Inject to top in BottomAppBarEventPresenter and MainActivity
    This commit moves the `@Inject` annotation to the top of the class definition in both `BottomAppBarEventPresenter` and `MainActivity` for better code style.

--- core/screens/src/main/java/me/sanao1006/screens/event/bottomAppBar/BottomAppBarEventPresenter.kt
+++ core/screens/src/main/java/me/sanao1006/screens/event/bottomAppBar/BottomAppBarEventPresenter.kt
@@ -3,10 +3,10 @@ package me.sanao1006.screens.event.bottomAppBar
 import androidx.compose.runtime.Composable
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.presenter.Presenter
+import javax.inject.Inject
 import me.sanao1006.core.data.compositionLocal.LocalNavigator
 import me.sanao1006.screens.HomeScreen
 import me.sanao1006.screens.NotificationScreen
-import javax.inject.Inject
 
 data class BottomAppBarState(
     val eventSink: (BottomAppBarActionEvent) -> Unit
