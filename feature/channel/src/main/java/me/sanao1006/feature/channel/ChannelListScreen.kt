1935833ce5b9da0664ce18bb9bbadbea05e0d336
sanao1006 <naosak1006@gmail.com>
2025-02-02 23:02:09 +0900

    use formatter

--- feature/channel/src/main/java/me/sanao1006/feature/channel/ChannelListScreen.kt
+++ feature/channel/src/main/java/me/sanao1006/feature/channel/ChannelListScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
