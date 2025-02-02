b124afd912f55984910769497460c8cd9dec1673
sanao1006 <naosak1006@gmail.com>
2025-02-02 21:10:24 +0900

    feat: add `limit` parameter to `GetChannelListUseCase` and `ChannelListRequestBody`
    The `GetChannelListUseCase` and `ChannelListRequestBody` have been updated to include a `limit` parameter.
    This change allows to control the number of channels returned in the response.
    The `limit` parameter defaults to 20.

--- core/domain/src/main/java/me/sanao1006/core/domain/channel/GetChannelListUseCase.kt
+++ core/domain/src/main/java/me/sanao1006/core/domain/channel/GetChannelListUseCase.kt
@@ -1,19 +1,21 @@
 package me.sanao1006.core.domain.channel
 
-import javax.inject.Inject
 import me.sanao1006.core.data.repository.ChannelRepository
 import me.sanao1006.core.model.notes.Channel
 import me.sanao1006.core.model.requestbody.channel.ChannelListRequestBody
+import javax.inject.Inject
 
 class GetChannelListUseCase @Inject constructor(
     private val channelRepository: ChannelRepository
 ) {
     suspend operator fun invoke(
-        query: String
+        query: String,
+        limit: Int = 20
     ): List<Channel> {
         return channelRepository.getChannelList(
             body = ChannelListRequestBody(
-                query = query
+                query = query,
+                limit = limit
             )
         )
     }
--- core/model/src/main/java/me/sanao1006/core/model/requestbody/channel/ChannelListRequestBody.kt
+++ core/model/src/main/java/me/sanao1006/core/model/requestbody/channel/ChannelListRequestBody.kt
@@ -4,5 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ChannelListRequestBody(
-    val query: String
+    val query: String,
+    val limit: Int
 )
